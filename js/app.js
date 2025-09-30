89a9fef2d5473b57f14ce0498d4bbe1838a046b7
userMauro-code <m88642336@gmail.com>
2025-09-29 22:10:31 -0300

    commit mobile finalized

--- js/app.js
+++ js/app.js
@@ -1,8 +1,15 @@
 const navBar = document.getElementById("navMenu");
 const btnNav = document.getElementById("btnMenu");
-const menuClose = document.querySelector(".menuClose");
-const openMenu = document.querySelector("openMenu");
+const btnIcon = btnNav.querySelector("img")
+
 
 btnNav.addEventListener('click', ()=>{
     navBar.classList.toggle("active");
+    if(btnNav.getAttribute("aria-expanded")==="false"){
+        btnNav.setAttribute("aria-expanded", "true");
+        btnIcon.setAttribute("src", "./assets/images/icon-menu-close.svg")
+    }else{
+        btnNav.setAttribute("aria-expanded", "false");
+        btnIcon.setAttribute("src", "./assets/images/icon-menu.svg");
+    }
 })
